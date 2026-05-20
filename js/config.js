b021e02a58613c2b91b613133f135f47b52c2843
nisansi <galvezneizan82@gmail.com>
2026-05-20 18:20:53 +0200

    imagen cocina

--- js/config.js
+++ js/config.js
@@ -160,6 +160,16 @@ const SITE_CONFIG = {
       },
       */
     ],
+    { id: 'cocina-benisano-1',
+        titulo: 'Reforma de Cocina',
+        ubicacion: 'Valencia',
+        categoria: 'cocina',
+        imagen: 'images/gallery_4.jpg',
+        imagenAlt: 'Cocina reformada en Valencia',
+        descripcion: 'Cocina abierta con isla central.',
+        destacado: true,
+        imagenes: ['images/gallery_4.jpg'],
+      }
   },
 
   proceso: {
